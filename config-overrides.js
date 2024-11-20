8fbe6bfb61639effc4b1560212c18a47c9fcea74
matty6223 <S3879003@student.rmit.edu.au>
2024-11-20 17:22:10 +1000

    fixed issues with @import deprecation warning

--- config-overrides.js
+++ config-overrides.js
@@ -6,16 +6,16 @@ module.exports = function override(config) {
     path: require.resolve('path-browserify'),
     os: require.resolve('os-browserify/browser'),
     crypto: require.resolve('crypto-browserify'),
-    buffer: require.resolve('buffer'), // Polyfill buffer
-    stream: require.resolve('stream-browserify'), // Polyfill stream
-    process: require.resolve('process/browser'), // Polyfill process
+    buffer: require.resolve('buffer'),
+    stream: require.resolve('stream-browserify'), 
+    process: require.resolve('process/browser'),
     vm: require.resolve("vm-browserify")
   };
 
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      process: 'process/browser', // Make process available globally
-      Buffer: ['buffer', 'Buffer'], // Make Buffer available globally
+      process: 'process/browser', 
+      Buffer: ['buffer', 'Buffer'],
     }),
   ]);
 
